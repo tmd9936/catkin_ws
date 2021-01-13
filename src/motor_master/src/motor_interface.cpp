f7b9c9502328d3f216fa5416a4b4a2b65bf30fb5
tmd9936 <you@example.com>
2021-01-13 10:09:52 +0900

    장애물 발견시 코드 수정중

--- src/motor_master/src/motor_interface.cpp
+++ src/motor_master/src/motor_interface.cpp
@@ -3,6 +3,10 @@
 #include "object_detection_gl_ros/Distance.h"
 #include <std_msgs/UInt16.h>
 
+#include <iostream> 
+#include <mutex>
+#include <std_msgs/Header.h>
+
 // 메인 모드
 #define DRIVE_MODE 0
 #define BLOCK_MODE 1
@@ -30,35 +34,40 @@ int left_dist = 0;
 int flag = 0;
 int sub_flag = 0;
 
+std::mutex  m; 
+
 void LidarCallback(const object_detection_gl_ros::Distance::ConstPtr &msg) {
 	front_dist = msg->front_dist;
 	left_dist = msg->left_dist;
-	ROS_INFO("front_distttt = %d cm", front_dist);
-	ROS_INFO("left_distttt = %d cm", left_dist);
+	//ROS_INFO("front_distttt = %d cm", front_dist);
+	//ROS_INFO("left_distttt = %d cm", left_dist);
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "motor_interface");
 	ros::NodeHandle nh;
-	
 
 	ros::Publisher station_stop_state = nh.advertise<std_msgs::UInt16>("station_stop_state", 1);
 	ros::Publisher motor_val = nh.advertise<std_msgs::UInt16>("motor_val", 1);
 	ros::Publisher servo_val = nh.advertise<std_msgs::UInt16>("servo_val", 1);
-	
-	
 
 	std_msgs::UInt16 motor_msg;
 	std_msgs::UInt16 servo_msg;
 
-
 	ros::Subscriber distance_sub = nh.subscribe("distances", 1, LidarCallback);	
 
-	ros::Rate loop_rate(30);	
+	ros::AsyncSpinner spinner(1);  //spin
+    spinner.start();
+    ros::Rate loop_rate(20);	
+
+	ros::Time begin_s;
 
 	while(ros::ok())
 	{
+		std::lock_guard<std::mutex> lock(m);
+
+		begin_s = ros::Time::now();
 		if(flag == DRIVE_MODE){
 			if(front_dist < 40 && front_dist > 0){
 				flag = BLOCK_MODE;
@@ -74,6 +83,7 @@ int main(int argc, char **argv)
 			//}
 			else
 			{
+				// 서보모터 각도 받아서 바꾸기
 				servo_msg.data = 75;
 				motor_msg.data = 30;
 			}
@@ -91,33 +101,34 @@ int main(int argc, char **argv)
 				{
 					motor_msg.data = 0;
 					sub_flag = RIGHT_TURN_MODD;
+					ros::Duration(0.1).sleep();
 				}
 			}
 			else if(sub_flag == RIGHT_TURN_MODD)
 			{
 				motor_msg.data = 0;
 				servo_msg.data = 120;
-				ros::Rate(5).sleep();
+				ros::Duration(3).sleep();
 				sub_flag = RT_AND_GO_MODE;
 				
 			}
 			else if(sub_flag == RT_AND_GO_MODE)
 			{
 				motor_msg.data = 30;
-				ros::Rate(2).sleep();
+				ros::Duration(1).sleep();
 				sub_flag = BLOCK_LEFT_TURN_MODE;
 			}
 			else if(sub_flag == BLOCK_LEFT_TURN_MODE)
 			{
 				motor_msg.data = 0;
 				servo_msg.data = 30;
-				ros::Rate(2).sleep();
+				ros::Duration(3).sleep();
 				sub_flag = BLOCK_LT_AND_GO_MODE;
 			}
 			else if(sub_flag == BLOCK_LT_AND_GO_MODE)
 			{
 				motor_msg.data = 30;
-				ros::Rate(1).sleep();
+				ros::Duration(1).sleep();
 				sub_flag = BLOCK_PASS_MODE;
 			}
 			else if(sub_flag == BLOCK_PASS_MODE)
@@ -125,7 +136,7 @@ int main(int argc, char **argv)
 				servo_msg.data = 75;
 				if(front_dist > 0 && front_dist < 15)
 				{
-					motor_msg.data = 30;
+					motor_msg.data = 15;
 				}
 				else
 				{
@@ -137,13 +148,13 @@ int main(int argc, char **argv)
 			{
 				motor_msg.data = 0;
 				servo_msg.data = 30;
-				ros::Rate(5).sleep();
+				ros::Duration(3).sleep();
 				sub_flag = LINE_LT_AND_GO_MODE;
 			}
 			else if(sub_flag == LINE_LT_AND_GO_MODE)
 			{
 				motor_msg.data = 30;
-				ros::Rate(2).sleep();
+				ros::Duration(1).sleep();
 				sub_flag = LINE_CHECK_MODE;
 			}
 			else if(sub_flag == LINE_CHECK_MODE)
@@ -153,25 +164,27 @@ int main(int argc, char **argv)
 
 				// if(line_state == 0) // opencv에서 받은 라인의 각도가 0이 되면 빠져나가기
 				//{
-				//  servo_msg.data = 75;
-				//  ros::Rate(500).sleep();
-				//	sub_flag = 0;
-				//	flag =  DRIVE_MODE;
+				ros::Duration(3).sleep();
+				servo_msg.data = 75;
+				ros::Duration(1).sleep();
+				sub_flag = 0;
+				flag = DRIVE_MODE;
 				//}
 
 			}
 		
 		}
+		ROS_INFO("flag = %d,  sub_flag = %d, sec = %f ", flag, sub_flag, ros::Time::now() - begin_s);
 
 		motor_val.publish(motor_msg);
 		servo_val.publish(servo_msg);
 		front_dist = 0; 
 	    loop_rate.sleep();
-		ros::spinOnce();
+		//ros::spinOnce();
 	
 	}
 
-
+	spinner.stop();
 
 	//ros::spin();
 
