bd4eab265ad5dcb32a8293e32339ec41f482e556
tmd9936 <you@example.com>
2021-01-13 18:13:21 +0900

    수정

--- src/motor_master/src/motor_interface.cpp
+++ src/motor_master/src/motor_interface.cpp
@@ -3,9 +3,11 @@
 #include "object_detection_gl_ros/Distance.h"
 #include <std_msgs/UInt16.h>
 
-#include <iostream> 
+#include <iostream>
 #include <mutex>
 #include <std_msgs/Header.h>
+
+#include <camera_opencv/TrafficState.h>
 
 // 메인 모드
 #define DRIVE_MODE 0
@@ -25,6 +27,13 @@
 #define LINE_CHECK_INIT_MODE 8
 #define LINE_CHECK_MODE 9
 
+// 신호등 색상
+#define RED 0
+#define GREEN 1
+
+#define STATION_AREA_OFF 0
+#define STATION_AREA_ON 4
+
 double right_turn_mode_duration;
 double rt_and_go_mode_duration;
 double block_left_turn_mode_duration;
@@ -38,18 +47,32 @@ double line_check_init_mode_duration;
 
 // STATION_MODE
 
+// 라이다 관련 변수
 int front_dist = 0;
 int left_dist = 0;
 
+// opencv 관령 변수
+int line_state = 0;
+int station_area = 0;
+int traffic_color = GREEN;
+
+// 모드
 int flag = 0;
+// 서브모드
 int sub_flag = 0;
 
+// 정지할 시간(초)
 float duration_sec = 0;
-
+// 기본 모터 속력
 int basic_motor_pwm = 30;
 
-std::mutex m; 
+std::mutex m;
 
+int line_state_control = 1;
+
+int pre_survo_val = 75;
+
+// launch 파일 파라미터
 void initParams(ros::NodeHandle *nh_priv)
 {
 	nh_priv->param("right_turn_mode_duration", right_turn_mode_duration, right_turn_mode_duration);
@@ -60,17 +83,30 @@ void initParams(ros::NodeHandle *nh_priv)
 	nh_priv->param("line_left_turn_mode_duration", line_left_turn_mode_duration, line_left_turn_mode_duration);
 	nh_priv->param("line_lt_and_go_mode_duration", line_lt_and_go_mode_duration, line_lt_and_go_mode_duration);
 	nh_priv->param("line_check_init_mode_duration", line_check_init_mode_duration, line_check_init_mode_duration);
-	
+
 	nh_priv->param("basic_motor_pwm", basic_motor_pwm, basic_motor_pwm);
 
+	nh_priv->param("line_state_control", line_state_control, line_state_control);
 }
 
-void LidarCallback(const object_detection_gl_ros::Distance::ConstPtr &msg) 
+void lidarCallback(const object_detection_gl_ros::Distance::ConstPtr &msg)
 {
 	front_dist = msg->front_dist;
 	left_dist = msg->left_dist;
 	//ROS_INFO("front_distttt = %d cm", front_dist);
 	//ROS_INFO("left_distttt = %d cm", left_dist);
+}
+
+void cameraCallback(const camera_opencv::TrafficState::ConstPtr &msg)
+{
+	line_state = msg->line_state;
+	station_area = msg->station_area;
+	traffic_color = msg->traffic_color;
+}
+
+void gettingOnStopCallback(condt std_msg::UIint8::ConstPtr &msg)
+{
+	return;
 }
 
 int main(int argc, char **argv)
@@ -86,140 +122,176 @@ int main(int argc, char **argv)
 	std_msgs::UInt16 motor_msg;
 	std_msgs::UInt16 servo_msg;
 
-	ros::Subscriber distance_sub = nh.subscribe("distances", 1, LidarCallback);	
+	camera_opencv::TrafficState traffic_state_msg;
 
-	ros::AsyncSpinner spinner(1);  //spin
-    spinner.start();
-    ros::Rate loop_rate(50);	
+	ros::Subscriber distance_sub = nh.subscribe("distances", 1, lidarCallback);
+	ros::Subscriber camera_opencv_sub = nh.subscribe("traffic_state", 1, cameraCallback);
+	//ros::Subscriber getting_on_stop_sub = nh.subscribe("getting_on_and_off_state", 1, gettingOnStopCallback)
+
+	ros::AsyncSpinner spinner(1); //spin
+	spinner.start();
+	ros::Rate loop_rate(50);
 
 	ros::Time begin_s;
 
 	initParams(&nh_priv);
 
-	while(ros::ok())
+	while (ros::ok())
 	{
 		std::lock_guard<std::mutex> lock(m);
 
 		begin_s = ros::Time::now();
-		if(flag == DRIVE_MODE){
+		// 주행모드
+		if (flag == DRIVE_MODE)
+		{
 			duration_sec = 0;
-			if(front_dist < 30 && front_dist > 0){
+			// 앞의 장애물이 30이하면 멈추고 장애물 감지 모드로 변경
+			if (front_dist < 30 && front_dist > 0)
+			{
 				flag = BLOCK_MODE;
 				sub_flag = 0;
 			}
-			//else if(traffic_light = 1)
-			//{
-			//	flag = TRAFFIC_LIGHT_MODE;
-			//}
-			//else if(station_on == 1)
-			//{
-			//	flag = STATION_MODE;
-			//}
+			else if (traffic_light = RED)
+			{
+				flag = TRAFFIC_LIGHT_MODE;
+				motor_msg.data = 0;
+			}
+			else if(station_on == STATION_AREA_ON)
+			{
+				flag = STATION_MODE;
+			}
 			else
 			{
 				// opencv에서 차선 각도 받아서 서보모터 각도 바꾸기
-				servo_msg.data = 75;
+				// 이상치가 오면 전의 값으로 대체
+				if(line_state > -30 && line_state < 30)
+				{
+					servo_msg.data = 75 + (line_state * line_state_control);
+				}  
+				else 
+				{
+					servo_msg.data = pre_survo_val;
+				}
+
+				pre_survo_val = servo_msg.data;
 				motor_msg.data = basic_motor_pwm;
+
 			}
 		}
-
-		if(flag == BLOCK_MODE)
+		// 장애물 만날경우
+		else if (flag == BLOCK_MODE)
 		{
-			if(sub_flag == DIST_INIT_MODE)
+			if (sub_flag == DIST_INIT_MODE)
 			{
 				motor_msg.data = 0;
 				sub_flag = RIGHT_TURN_MODE;
 				duration_sec = 1.5;
 			}
-			else if(sub_flag == RIGHT_TURN_MODE)
+			else if (sub_flag == RIGHT_TURN_MODE)
 			{
 				motor_msg.data = 0;
 				servo_msg.data = 120;
 				duration_sec = right_turn_mode_duration;
 				sub_flag = RT_AND_GO_MODE;
-				
 			}
-			else if(sub_flag == RT_AND_GO_MODE)
+			else if (sub_flag == RT_AND_GO_MODE)
 			{
 				motor_msg.data = basic_motor_pwm;
 				duration_sec = rt_and_go_mode_duration;
 				sub_flag = BLOCK_LEFT_TURN_MODE;
 			}
-			else if(sub_flag == BLOCK_LEFT_TURN_MODE)
+			else if (sub_flag == BLOCK_LEFT_TURN_MODE)
 			{
 				motor_msg.data = 0;
 				servo_msg.data = 30;
 				duration_sec = block_left_turn_mode_duration;
 				sub_flag = BLOCK_LT_AND_GO_MODE;
 			}
-			else if(sub_flag == BLOCK_LT_AND_GO_MODE)
+			else if (sub_flag == BLOCK_LT_AND_GO_MODE)
 			{
 				motor_msg.data = basic_motor_pwm;
 				duration_sec = block_lt_and_go_mode_duration;
 				sub_flag = BLOCK_PASS_MODE;
 			}
-			else if(sub_flag == BLOCK_PASS_MODE)
+			else if (sub_flag == BLOCK_PASS_MODE)
 			{
 				servo_msg.data = 75;
-				if(left_dist > 0 && left_dist <= 15)
+				if (left_dist > 0 && left_dist <= 15)
 				{
 					motor_msg.data = basic_motor_pwm;
 					duration_sec = 0;
 				}
-				else if(left_dist > 15)
+				else if (left_dist > 15)
 				{
 					motor_msg.data = 0;
 					duration_sec = block_pass_mode_duration;
 					sub_flag = LINE_LEFT_TURN_MODE;
 				}
-				else {}
+				else
+				{
+				}
 			}
-			else if(sub_flag == LINE_LEFT_TURN_MODE)
+			else if (sub_flag == LINE_LEFT_TURN_MODE)
 			{
 				motor_msg.data = 0;
 				servo_msg.data = 30;
 				duration_sec = line_left_turn_mode_duration;
 				sub_flag = LINE_LT_AND_GO_MODE;
 			}
-			else if(sub_flag == LINE_LT_AND_GO_MODE)
+			else if (sub_flag == LINE_LT_AND_GO_MODE)
 			{
 				motor_msg.data = basic_motor_pwm;
 				duration_sec = line_lt_and_go_mode_duration;
 				sub_flag = LINE_CHECK_INIT_MODE;
 			}
-			else if(sub_flag == LINE_CHECK_INIT_MODE)
+			//차선 보일때까진 움직이기
+			else if (sub_flag == LINE_CHECK_INIT_MODE)
 			{
 				motor_msg.data = basic_motor_pwm;
 				servo_msg.data = 120;
-				duration_sec = line_check_init_mode_duration; //차선 보일때까진 움직이기
-				sub_flag = LINE_CHECK_MODE
+				duration_sec = line_check_init_mode_duration;
+				sub_flag = LINE_CHECK_MODE;
 			}
-			else if(sub_flag == LINE_CHECK_MODE)
-			{				
-				// if(line_state == 0) // opencv에서 받은 라인의 각도가 0이 되면 빠져나가기
-				//{
+			else if (sub_flag == LINE_CHECK_MODE)
+			{
+				// opencv에서 받은 라인의 각도가 0이 되면 빠져나가기
+				if(line_state == 0) 
+				{
 					motor_msg.data = 0;
 					servo_msg.data = 75;
 					duration_sec = 1;
 					sub_flag = 0;
 					flag = DRIVE_MODE;
-				//}
+				}
 			}
-		
 		}
-		ROS_INFO("%f", block_left_turn_mode_duration);
-		ROS_INFO("flag = %d,  sub_flag = %d, sec = %f ", flag, sub_flag, ros::Time::now() - begin_s);
+		// 빨간불을 만날 경우 대기 모드
+		else if (flag == TRAFFIC_LIGHT_MODE)
+		{
+			motor_msg.data = 0;
+			if (traffig_light == GREEN)
+			{
+				flag = DRIVE_MODE;
+			}
+			else {}
+
+		}
+		else if (flag == STATION_MODE)
+		{
+			
+		}
+		else {}
+
+		ROS_INFO("flag = %d,  sub_flag = %d, sec = %f ", flag, sub_flag, ros::Time::now().toSec() - begin_s.toSec());
 
 		motor_val.publish(motor_msg);
 		servo_val.publish(servo_msg);
-		front_dist = 0; 
+		front_dist = 0;
 		ros::Duration(duration_sec).sleep();
-		ROS_INFO("left distance %d", left_dist);
+		//ROS_INFO("left distance %d", left_dist);
 
 		ros::spinOnce();
-	    loop_rate.sleep();
-		
-	
+		loop_rate.sleep();
 	}
 
 	spinner.stop();
@@ -228,6 +300,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-
-
